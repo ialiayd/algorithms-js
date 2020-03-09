db85163afb08e1256ff07e08b6247e93b5bc1dc3
ali.aydemir <ali.aydemir1993@gmail.com>
2020-03-09 14:20:16 +0300

    first commit

---
+++ reversestring/index.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {}
+
+module.exports = reverse;
+
+//testleri çalıştırmak için jest test paketini npmden yüklemek gerekir. 
+// egzersiz dosyasına geç > jest komutunu çalıştır.
+//jest reversestring/test.js --watch
+//hata alırsak da jest reversestring(klasör adı) --watch olarak dene
+//watchdan çıkmadığımız sürece kodda değişiklik yaptıkça test koşmaya devam eder.
